3d47b45f1d50368c6561f911ea8aa15f103cfb6a
vladapascal <166646935+vladapascal@users.noreply.github.com>
2024-04-10 15:00:16 -0400

    Add files via upload

---
+++ task3lesson4.py
@@ -0,0 +1,49 @@
+# Aceasta este a treia ta sarcină a lecției legată de type conversion și input-ul user-ului în python
+
+# Creează o variabilă numită `number` și atribuie-i valoarea `10`
+
+# CODUL TĂU VINE MAI JOS:
+number = 10
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează valoarea variabilei `number` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(number)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum cere user-ului să introducă un număr și atribuie acea valoare variabilei `number` și afișeaz-o folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+number = input("Introduceți un număr: ")
+print(number)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează tipul variabilei `number` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(type(number))
+# CODUL TĂU VINE MAI SUS:
+
+# Convertește variabila `number` la tipul `float` și afișează tipul variabilei `number` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+number = float(number)
+print(type(number))
+# CODUL TĂU VINE MAI SUS:
+
+# Convertește variabila `number` la tipul `str` și afișează tipul variabilei `number` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+number = str(number)
+print(type(number))
+# CODUL TĂU VINE MAI SUS:
+
+# Convertește variabila `number` la tipul `bool` și afișează tipul variabilei `number` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+number = bool(number)
+print(type(number))
+# CODUL TĂU VINE MAI SUS:
+
+# Asta a fost tot pentru această sarcină
